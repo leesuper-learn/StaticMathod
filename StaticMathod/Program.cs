36c95b0cf309764827c0ff2120a4813fe4852066
Sillver Bullet <161559713+leesuper-learn@users.noreply.github.com>
2024-03-09 14:45:32 +0800

    Initial commit

---
+++ StaticMathod/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMathod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Change();//calling change method
+                             //creating objects
+            Student s1 = new Student(11, "Name 1");
+            Student s2 = new Student(21, "Name 2");
+            Student s3 = new Student(31, "Name 3");
+            //calling display method
+            s1.Display();
+            s2.Display();
+            s3.Display();
+            Console.ReadKey();
+        }
+        public class Student
+        {
+            private int rollno;
+            private string name;
+            private static string college = "BBDIT";
+
+            //constructor to initialize the variable
+            public Student(int r, string n)
+            {
+                rollno = r;
+                name = n;
+            }
+
+            //static method to change the value of static variable
+
+            public static void Change()
+            {
+                college = "Test";
+            }
+
+            //method to display values
+            public void Display()
+            {
+                Console.WriteLine(rollno + " " + name + " " + college);
+            }
+        }
+    }
+}
